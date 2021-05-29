48121707d69a477367575ce12571125bea7b9830
Pintapoff <anders@billfors.com>
2021-05-29 23:38:18 +0200

    version 0.1.0

--- index.js
+++ index.js
@@ -1,45 +1,107 @@
-'use strict';
+const Domain = require('./lib/domain.js');
+const Network = require('./lib/network.js');
 
-const api = require('./lib');
-const Domain = new api.domain(process.env.DO_API_KEY);
-const Network = new api.network();
+class Updater {
 
-async function test(rec, dom){
-    try {       
-        let result = await Promise.all([
-                            Domain.getRecord(rec, dom),
-                            Network.getPublicIP()
-                            ]);
+    constructor(key) {
+        this.domain = new Domain(key);
+        this.network = new Network();
+    }
+    /**
+     * Sets the IP for a specific subdomain, will default to resolving your current external IP if none is supplied. (using ipinfo.io as resolver).
+     * @param {string} subdomain - The record/subdomain we want to set IP on.
+     * @param {string} domain - The domain we want to set IP on.
+     * @param {string} [ip=null] - Optional IP to set if you do not want your current external IP.
+     * @param {boolean} [debug=false] - Optional boolean to display some console logs.
+     */
+    async setIP(subdomain, domain, ip = null, debug = false) {
 
-        let record = result[0];
-        let ip = result[1];
+        const returnObject = {
+            success: true,
+            subdomain: `${subdomain}.${domain}`,
+            ip: null,
+            message: ''
+        };
 
-        console.log('External IP:', ip, ', Registered IP:', record.data);
-
-        if(record.data == ip) {
-            console.log('The same IP, no update.');
+        //This should mean that we skipped the ip parameter but supplied the debug boolean.
+        if (typeof ip === "boolean") {
+            debug = ip
         }
-        else {
-            console.log('New IP, will update.');            
-            let putData = {data: ip}
-            let updateDomain = await Domain.updateRecord(dom, record.id, putData);
 
-            if(updateDomain.domain_record.data == ip) {
-                console.log('Update done.');
+        try {
+            let record, recordIp;
+
+            if (ip) {
+                let result = await this.domain.getRecord(subdomain, domain);
+
+                record = result;
+                recordIp = ip;
+
+                if (debug)
+                    console.log(`Input IP: ${recordIp}, Registered IP: ${record.data}`)
+            } else {
+                let result = await Promise.all([
+                    this.domain.getRecord(subdomain, domain),
+                    this.network.getPublicIP()
+                ]);
+
+                record = result[0];
+                recordIp = result[1];
+
+                if (debug)
+                    console.log(`External IP: ${recordIp}, Registered IP: ${record.data}`)
+            }
+
+            if (record.data == recordIp) {
+
+                returnObject.message = `Same IP, no update needed.`;
+                returnObject.ip = record.data;
+
+                if (debug)
+                    console.log(returnObject.message);
             }
             else {
-                if(typeof updateDomain.domain_record !== 'object') {
-                    console.log('Update could not be performed, no domain record object returned.');
+                if (debug)
+                    console.log(`New IP, will update.`);
+
+                let putData = { data: recordIp }
+                let updateDomain = await this.domain.updateRecord(domain, record.id, putData);
+
+                if (updateDomain.domain_record.data == recordIp) {
+
+                    returnObject.message = `Update done.`;
+                    returnObject.ip = updateDomain.domain_record.data;
+
+                    if (debug)
+                        console.log(returnObject.message);
                 }
                 else {
-                    console.log('Update could not be performed,', updateDomain);
-                }                
-            }            
-        }        
-    }
-    catch(err) {
-        console.log(err);
+                    returnObject.success = false;
+
+                    if (typeof updateDomain.domain_record !== 'object') {
+
+                        returnObject.message = `Update could not be performed, no domain record object returned.`;
+
+                        if (debug)
+                            console.log(returnObject.message);
+                    }
+                    else {
+                        returnObject.message = `Update could not be performed, unknown error`;
+
+                        if (debug)
+                            console.log(returnObject.message, updateDomain);
+                    }
+                }
+            }
+
+            return returnObject;
+        }
+        catch (err) {
+            returnObject.message = err;
+            returnObject.success = false;
+            return returnObject;
+        }
     }
 }
 
-test('test', 'test.eu');
+module.exports = Updater;
